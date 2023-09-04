86c647c320d0ad624e29d847b0b11da4ce50a9b4
Olga Kutueva <kutueva.olga@gmail.com>
2023-09-04 14:48:50 +0300

    Homework 2. Task 1

---
+++ HOMEWORK_2/index.js
@@ -0,0 +1,6 @@
+alert('Привет! Я - задача номер 1!');
+
+let numFirst = prompt('Первая переменная:');
+let numSecond = prompt('Вторая переменная:');
+alert((numFirst <= 1) ? `${numFirst} \u2264 1` : `${numFirst} > 1`);
+alert((numSecond >= 3) ? `${numSecond} \u2265 3` : `${numSecond} < 3`);
