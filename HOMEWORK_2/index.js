f9cf96681fa91eb2891c83d47ff80b65ff7f3fbb
Olga Kutueva <kutueva.olga@gmail.com>
2023-09-04 14:49:43 +0300

    Homework 2. Task 2

--- HOMEWORK_2/index.js
+++ HOMEWORK_2/index.js
@@ -4,3 +4,8 @@ let numFirst = prompt('Первая переменная:');
 let numSecond = prompt('Вторая переменная:');
 alert((numFirst <= 1) ? `${numFirst} \u2264 1` : `${numFirst} > 1`);
 alert((numSecond >= 3) ? `${numSecond} \u2265 3` : `${numSecond} < 3`);
+
+alert('Я - задача номер 2!');
+let test = true;
+console.log(((test === true) ? '+++' : '---'));
+alert('Ответ - в коде и в консоли!');
