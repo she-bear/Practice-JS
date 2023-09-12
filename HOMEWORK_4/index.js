70cc5864b1786e67a2926dc0b501dc0a5bf1ed26
Olga Kutueva <kutueva.olga@gmail.com>
2023-09-12 10:32:17 +0300

    Homework 4. Task 2

--- HOMEWORK_4/index.js
+++ HOMEWORK_4/index.js
@@ -4,3 +4,8 @@ console.log("0 - это ноль");
 for (let index = 1; index < arrayLength; index++) {
     (index % 2 === 0) ? console.log(`${index} - четное число`) : console.log(`${index} - нечетное число`);
 }
+
+// Task 2
+const arrayIn = [1, 2, 3, 4, 5, 6, 7];
+arrayIn.splice(3, 2);
+console.log(arrayIn);
