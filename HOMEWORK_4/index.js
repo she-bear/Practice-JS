390c603961677701c3a0c5541086ccd8ad4d9c14
Olga Kutueva <kutueva.olga@gmail.com>
2023-09-12 10:43:45 +0300

    Homework 4. Task 3

--- HOMEWORK_4/index.js
+++ HOMEWORK_4/index.js
@@ -9,3 +9,15 @@ for (let index = 1; index < arrayLength; index++) {
 const arrayIn = [1, 2, 3, 4, 5, 6, 7];
 arrayIn.splice(3, 2);
 console.log(arrayIn);
+
+// Task 3
+const arrayRandomLength = 5;
+const arrayRandom = Array.from({ length: arrayRandomLength }, (el) => Math.floor(Math.random() * 10));
+console.log(arrayRandom);
+
+console.log(`Сумма элементов: ${arrayRandom.reduce((sum, item) => sum + item)}`);
+
+console.log(`Минимальное число: ${arrayRandom.reduce((min, item) => { if (min > item) return item; else return min })}`);
+
+const numberForFind = 3;
+(arrayRandom.includes(numberForFind)) ? console.log(`В массиве есть цифра ${numberForFind}`) : console.log(`В массиве нет цифры ${numberForFind}`);
