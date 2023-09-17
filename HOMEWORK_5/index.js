3692bf3a0c7e2326b7a5981e6d48df3fd4380e24
Olga Kutueva <kutueva.olga@gmail.com>
2023-09-17 14:27:37 +0300

    Homework 5. Задание 1

---
+++ HOMEWORK_5/index.js
@@ -0,0 +1,11 @@
+// task 1
+const numbers = {
+    keyin1: 1,
+    keyin2: 2,
+    keyin3: 3,
+    keyin4: 4,
+    keyin5: 5,
+    keyin6: 6,
+    keyin7: 7,
+}
+console.log(Object.values(numbers).filter((item) => item >= 3));
