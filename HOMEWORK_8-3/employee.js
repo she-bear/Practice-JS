a3b9db28a78b277bd4c980f6e4445e793cf04d0d
Olga Kutueva <kutueva.olga@gmail.com>
2023-09-24 22:28:17 +0300

    Homework_8-3. Задание 1

--- HOMEWORK_8-3/employee.js
+++ HOMEWORK_8-3/employee.js
@@ -5,3 +5,30 @@
 Реализуйте класс Manager (менеджер), который наследует класс Employee и имеет дополнительное свойство и метод:
 Свойство department (отдел) - строка, отдел, в котором работает менеджер.
 Метод displayInfo() - переопределяет метод displayInfo() родительского класса и выводит информацию о менеджере (имя и отдел). */
+
+class Employee {
+    constructor(name) {
+        this.name = name;
+    }
+
+    displayInfo() {
+        console.log(`Name: ${this.name}`);
+    }
+}
+
+class Manager extends Employee {
+    constructor(name, department) {
+        super(name);
+        this.department = department;
+    }
+
+    displayInfo() {
+        console.log(`Name: ${this.name}, department: ${this.department}`);
+    }
+}
+
+const employee = new Employee("John Smith");
+employee.displayInfo();
+
+const manager = new Manager("Jane Doe", "Sales");
+manager.displayInfo();
