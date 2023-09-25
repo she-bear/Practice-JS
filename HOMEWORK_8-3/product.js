f9bcf0849abbd075fdcf4048bb164ba399c3b4d4
Olga Kutueva <kutueva.olga@gmail.com>
2023-09-25 18:18:50 +0300

    Homework_8-3. Задание 2

--- HOMEWORK_8-3/product.js
+++ HOMEWORK_8-3/product.js
@@ -1,3 +1,4 @@
+'use strict';
 /* Реализуйте класс Product (товар), который имеет следующие свойства и методы:
 Свойство name - название товара.
 Свойство price - цена товара.
@@ -8,3 +9,58 @@
 Свойство products (продукты) - массив, содержащий список продуктов в заказе.
 Метод addProduct(product) - принимает объект класса Product и добавляет его в список продуктов заказа.
 Метод getTotalPrice() - возвращает общую стоимость заказа, основанную на ценах продуктов. */
+
+class Rodent {
+    constructor(type, breed, name) {
+        this.type = type;
+        this.breed = breed;
+        this.name = name;
+    }
+
+    displayInfo() {
+        console.log(`Hi, I am ${this.breed} ${this.type}! My name is ${this.name}`);
+    }
+}
+
+class Product {
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
+
+class Order {
+    constructor(id) {
+        this.name = id;
+    }
+
+    products = [];
+
+    addProduct(product) {
+        if (Object.getPrototypeOf(product) != Product.prototype) {
+            throw new Error(`Oh, now! It isn't a product, it is a ${product.constructor.name}`)
+        }
+        this.products.push(product);
+    }
+    getTotalPrice() {
+        return this.products.reduce((total, item) => item.price * item.quantity + total, 0);
+    }
+}
+
+const order = new Order(12345);
+
+const productOne = new Product("Phone", 500, 2);
+order.addProduct(productOne);
+
+const productTwo = new Product("Headphones", 100, 1);
+order.addProduct(productTwo);
+
+console.log(order.getTotalPrice()); // Вывод: 1100
+
+const humsterSurprise = new Rodent("Humster", "Grey dwarf", "Surprise");
+humsterSurprise.displayInfo();
+order.addProduct(humsterSurprise); // exception
+
+
+
